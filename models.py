da5b92858a1899ba29483600c142cbbc52850c75
Linda Ebbert <linda_ebbert@yahoo.de>
2026-08-16 19:33:29 +0200

    fix: adjust nullability for movie model fields

--- models.py
+++ models.py
@@ -21,10 +21,10 @@ class Movie(db.Model):
     """Movie model."""
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
-    director = db.Column(db.String(100), nullable=False)
-    year = db.Column(db.Integer, nullable=False)
-    poster_url = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    director = db.Column(db.String(100))
+    year = db.Column(db.Integer)
+    poster_url = db.Column(db.String(255))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
