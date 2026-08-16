78e6d4e64f5207d959c81b39e903e8a4525bf658
Linda Ebbert <linda_ebbert@yahoo.de>
2026-08-16 23:07:44 +0200

    feat: add templates, routes and styling for user management

--- app.py
+++ app.py
@@ -17,14 +17,31 @@ data_manager = DataManager()
 
 
 @app.route("/", methods=["GET"])
-def home():
-    return "Welcome to MoviWeb App!"
-
-
-@app.route("/users", methods=["GET"])
-def list_users():
+def index():
+    """Render the index page."""
     users = data_manager.get_users()
-    return {"users": [user.name for user in users]}
+    return render_template("index.html", users=users)
+
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    """Create a new user. If the name is empty,
+    redirect to the index.
+    """
+
+    user_name = request.form.get("name","").strip()
+    if not user_name:
+        return redirect(url_for("index"))
+
+    data_manager.create_user(user_name)
+    return redirect(url_for("index"))
+
+
+@app.route('/users/<int:user_id>/movies')
+def get_movies(user_id):
+    """Get movies for a user."""
+
+    return f"Movies for user: {user_id}"
 
 
 if __name__ == '__main__':
